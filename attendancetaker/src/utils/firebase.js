66d6492fa56f99229d339735a44bdb47db33ea27
William Bucher <wjbucher3@gmail.com>
2022-01-02 06:28:52 -0500

    cleaned up firebase.js

--- attendancetaker/src/utils/firebase.js
+++ attendancetaker/src/utils/firebase.js
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, set  } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
