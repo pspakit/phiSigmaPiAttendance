34bfea954ed28cbbe339d8dcced51ffadb754e5e
bthall73 <bthall@live.unc.edu>
2022-07-27 13:42:03 -0400

    Updated the firebase configuration code and created a new database

--- attendancetaker/src/utils/firebase.js
+++ attendancetaker/src/utils/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyCp_3hdrUprDdmoI9O3z1i6OwAJK_JArYg",
   authDomain: "phisigmapiattendance-6b5cd.firebaseapp.com",
   projectId: "phisigmapiattendance-6b5cd",
@@ -14,6 +14,17 @@ const firebaseConfig = {
   appId: "1:254916396235:web:6108e74f814a8de4a6c74e",
   measurementId: "G-QVR4D71SRK",
   databaseURL: "https://phisigmapiattendance-6b5cd-default-rtdb.firebaseio.com/"
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXYP3_-5w7DrZ_fzVLoQ6ftgjd6uNCUeE",
+  authDomain: "phi-sigma-pi-attendance.firebaseapp.com",
+  databaseURL: "https://phi-sigma-pi-attendance-default-rtdb.firebaseio.com",
+  projectId: "phi-sigma-pi-attendance",
+  storageBucket: "phi-sigma-pi-attendance.appspot.com",
+  messagingSenderId: "355174515625",
+  appId: "1:355174515625:web:cc3e4ecdb351c58c21fab8",
+  measurementId: "G-2ZYQ4RTP48"
 };
 
 // Initialize Firebase
