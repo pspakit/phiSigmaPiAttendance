957a3847919334e6bce63f4cad326682cd7d022a
Andrew Lockard <alockard@unc.edu>
2024-08-29 20:43:02 -0400

    Add Excuse Form Button

--- attendancetaker/src/components/accountSelection.js
+++ attendancetaker/src/components/accountSelection.js
@@ -10,6 +10,10 @@ export default function AccountSelection() {
             <h1>Phi Sig Attendance</h1>
             <button onClick={() => navigate('/login')}>Login</button>
             <button onClick={() => navigate('/register')}>Register</button>
+            
+            <br></br>
+            <h2>Absent?</h2>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSe_7KowF93kMQzNaV36cXXNOAt5TGSHH-NjxndHvfqO4axHFQ/viewform"><button>Excuse Form</button></a>
 
         </div>
     );
